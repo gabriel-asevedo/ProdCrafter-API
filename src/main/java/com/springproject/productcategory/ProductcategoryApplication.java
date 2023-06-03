a7e5d439d9d6f6ca7e112f870da6bdc41db89841
Gabriel Asevedo <gabrielamorim002@gmail.com>
2023-06-02 21:00:42 -0300

    Product memory

--- src/main/java/com/springproject/productcategory/ProductcategoryApplication.java
+++ src/main/java/com/springproject/productcategory/ProductcategoryApplication.java
@@ -1,18 +1,25 @@
 package com.springproject.productcategory;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.springproject.productcategory.entities.Category;
+import com.springproject.productcategory.entities.Product;
 import com.springproject.productcategory.repositories.CategoryRepository;
+import com.springproject.productcategory.repositories.ProductRepository;
 
 @SpringBootApplication
 public class ProductcategoryApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoryRepository categoryRepository;
+
+	@Autowired
+	private ProductRepository productRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductcategoryApplication.class, args);
@@ -24,8 +31,21 @@ public class ProductcategoryApplication implements CommandLineRunner {
 		Category cat1 = new Category(1L, "Electronics");
 		Category cat2 = new Category(2L, "Books");
 
+		Product p1 = new Product(1L, "TV", 3799.00, cat1);
+		Product p2 = new Product(2L, "Clean Code", 86.58, cat2);
+		Product p3 = new Product(3L, "PS5", 4299.00, cat1);
+		Product p4 = new Product(4L, "Modern Software Engineering", 120.00, cat2);
+
+		cat1.getProducts().addAll(Arrays.asList(p1, p3));
+		cat2.getProducts().addAll(Arrays.asList(p2, p4));
+
 		categoryRepository.save(cat1);
 		categoryRepository.save(cat2);
+
+		productRepository.save(p1);
+		productRepository.save(p2);
+		productRepository.save(p3);
+		productRepository.save(p4);
 
 	}
 
--- src/main/java/com/springproject/productcategory/entities/Category.java
+++ src/main/java/com/springproject/productcategory/entities/Category.java
@@ -1,13 +1,20 @@
 package com.springproject.productcategory.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class Category implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private Long id;
 	private String name;
+
+	@JsonIgnore
+	private List<Product> products = new ArrayList<>();
 
 	public Category() {
 	}
@@ -36,6 +43,10 @@ public class Category implements Serializable {
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
+	}
+
+	public List<Product> getProducts() {
+		return products;
 	}
 
 	@Override
